1edc1b716268e2c2e529247d7011cda6a400e279
Lucas Wendel <lucas.wmartins@hotmail.com>
2021-05-30 17:40:45 -0300

    Finished section 09 Database And Newsletter

--- Seção 08/LojaVirtual/LojaVirtual/Controllers/HomeController.cs
+++ Seção 08/LojaVirtual/LojaVirtual/Controllers/HomeController.cs
@@ -7,14 +7,41 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
+using LojaVirtual.Database;
 
 namespace LojaVirtual.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly LojaVirtualContext _context;
+
+        public HomeController(LojaVirtualContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Index([FromForm]NewsletterEmail newsletter)
+        {
+            if (ModelState.IsValid)
+            {
+                //TODO - Adição no banco de dados
+                _context.NewsletterEmails.Add(newsletter);
+                _context.SaveChanges();
+
+                TempData["MSG_S"] = "E-mail cadastrado! Agora você irá receber promoções especiais no seu e-mail! Fique atento as novidades!";
+
+                return RedirectToAction(nameof(Index));
+            } else
+            {
+                return View();
+            }
+
         }
 
         public IActionResult Contato()
